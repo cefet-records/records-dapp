3a5549f8f98a419bfc9e7e565cfa7239a18407af
jsuisjuan <juanc.s.delima@gmail.com>
2026-01-10 23:09:25 -0300

    feat: adding polygon config

--- src/config/chains.config.ts
+++ src/config/chains.config.ts
@@ -1,18 +1,35 @@
-export const evmNetworks = [
-    {
-        blockExplorerUrls: [],
-        chainId: 31337,
-        chainName: 'Hardhat Localnet',
-        iconUrls: [],
-        name: 'Hardhat',
-        nativeCurrency: {
-            decimals: 18,
-            name: 'Ether',
-            symbol: 'ETH',
-            iconUrl: '',
-        },
-        networkId: 31337,
-        rpcUrls: ['http://127.0.0.1:8545'],
-        vanityName: 'Hardhat',
+import { EvmNetwork } from "@dynamic-labs/sdk-react-core"
+
+export const evmNetworks: EvmNetwork[] = [
+{
+    blockExplorerUrls: ['https://polygonscan.com/'],
+    chainId: 137,
+    chainName: 'Matic Mainnet',
+    iconUrls: ["https://app.dynamic.xyz/assets/networks/polygon.svg"],
+    name: 'Polygon',
+    nativeCurrency: {
+      decimals: 18,
+      name: 'MATIC',
+      symbol: 'MATIC',
+      iconUrl: 'https://app.dynamic.xyz/assets/networks/polygon.svg',
     },
-];
+    networkId: 137,
+    rpcUrls: ['https://polygon-rpc.com'],
+    vanityName: 'Polygon',
+  },
+  {
+    chainId: 31337,
+    networkId: 31337,
+    chainName: 'Hardhat Localnet',
+    name: 'Hardhat',
+    vanityName: 'Hardhat',
+    iconUrls: [],
+    nativeCurrency: {
+      name: 'Ether',
+      symbol: 'ETH',
+      decimals: 18,
+    },
+    rpcUrls: ['http://127.0.0.1:8545'],
+    blockExplorerUrls: [],
+  },
+]
--- src/config/wagmi.config.ts
+++ src/config/wagmi.config.ts
@@ -1,18 +1,35 @@
-import { createConfig, http } from 'wagmi';
-import { mainnet } from 'wagmi/chains';
-import { getOrMapViemChain} from '@dynamic-labs/ethereum-core';
-import { evmNetworks } from './chains.config';
-import { createClient } from 'viem';
+import { createConfig, http } from 'wagmi'
+import { getOrMapViemChain } from '@dynamic-labs/ethereum-core'
+import { evmNetworks } from './chains.config'
+import { createClient } from 'viem'
+
+const isDevelopment = false
+const POLYGON_RPC = process.env.NEXT_PUBLIC_POLYGON_RPC;
+const HARDHAT_RPC = 'http://127.0.0.1:8545';
+
+const polygonNetwork = getOrMapViemChain(
+  evmNetworks.find(n => n.chainId === 137)!
+)
+
+const hardhatNetwork = evmNetworks.find(n => n.chainId === 31337)
+  ? getOrMapViemChain(
+      evmNetworks.find(n => n.chainId === 31337)!
+    )
+  : undefined
 
 export const config = createConfig({
-    chains: [
-        mainnet,
-        ...evmNetworks.map(getOrMapViemChain)
-    ],
-    client({ chain }) {
+  chains: isDevelopment && hardhatNetwork
+    ? [polygonNetwork, hardhatNetwork]
+    : [polygonNetwork],
+
+  client({ chain }) {
     return createClient({
       chain,
-      transport: http(),
-    });
+      transport: http(
+        chain.id === 31337
+          ? HARDHAT_RPC
+          : POLYGON_RPC
+      ),
+    })
   },
 });
